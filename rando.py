ea9da041379936a9a07ae3890053bfdc304e98a0
Feyaz Baker <feyazabubaker@gmail.com>
2021-03-11 12:08:54 +0530

    Script of random num gen

---
+++ rando.py
@@ -0,0 +1,11 @@
+import random
+import numpy as np
+vals = []
+
+for i in range(100000):
+    vals.append(np.round(np.random.normal(0, scale=1/6)))
+
+val = set(vals)
+
+for x in val:
+    print(x," ", vals.count(x))
