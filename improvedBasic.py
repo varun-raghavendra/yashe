f50cf091f48e794c8f6d0116ee9b012a3e3452bd
Nuthan Kumar S <nuthankumars10@gmail.com>
2021-03-15 20:22:26 +0530

    changed reduce functions

--- improvedBasic.py
+++ improvedBasic.py
@@ -24,57 +24,76 @@ exp= 2.718281828459045
 B = 18
 sigma = np.floor(B/6)
 
-def reduce(p):
-    # Put the given polynomial in the range [-q/2, q/2)
+# def reduce(p):
+#     # Put the given polynomial in the range [-q/2, q/2)
+#     global phid
+#     global q
+#     p = [x % q for x in p]
+#     # print(p)
+#     Q, R = np.polydiv(p, phid)
+#     R = [x % q for x in R]
+#     for i in range(len(R)):
+#       if R[i]>q//2:
+#         R[i]-=q
+#     return R
+#
+# def reduce_t(p):
+#     # Put the given polynomial in the range [-t/2, t/2)
+#     global phid
+#     global t
+#     p = [x % t for x in p]
+#     # print(p)
+#     Q, R = np.polydiv(p, phid)
+#     R = [x % t for x in R]
+#     for i in range(len(R)):
+#       if R[i]>t//2:
+#         R[i]-=t
+#     return R
+#
+# def reduceMod(p):
+#     # Put the given polynomial in the range [-0, q)
+#     global phid
+#     global q
+#     p = [x % q for x in p]
+#     # print(p)
+#     Q, R = np.polydiv(p, phid)
+#     R = [x % q for x in R]
+#
+#     return R
+#
+# def reduceMod_t(p):
+#     # Put the given polynomial in the range [-0, t)
+#     global phid
+#     global t
+#     p = [x % t for x in p]
+#     # print(p)
+#     Q, R = np.polydiv(p, phid)
+#     R = [x % t for x in R]
+#
+#     return R
+
+def reduce(p, mod, centered=True, divide=True):
+    # Put the given polynomial in the range [-mod/2, mod/2) if centered,
+    # and to [0,mod) if not centered.
     global phid
-    global q
-    p = [x % q for x in p]
+
+    R = [x % mod for x in p]
     # print(p)
-    Q, R = np.polydiv(p, phid)
-    R = [x % q for x in R]
-    for i in range(len(R)):
-      if R[i]>q//2:
-        R[i]-=q
+    if divide:
+        Q, R = np.polydiv(p, phid)
+        R = [x % mod for x in R]
+
+    if centered:
+        for i in range(len(R)):
+            if R[i]>mod//2:
+                R[i]-=mod
+
     return R
 
-def reduce_t(p):
-    # Put the given polynomial in the range [-t/2, t/2)
-    global phid
-    global t
-    p = [x % t for x in p]
-    # print(p)
-    Q, R = np.polydiv(p, phid)
-    R = [x % t for x in R]
-    for i in range(len(R)):
-      if R[i]>t//2:
-        R[i]-=t
-    return R
-
-def reduceMod(p):
-    # Put the given polynomial in the range [-0, q)
-    global phid
-    global q
-    p = [x % q for x in p]
-    # print(p)
-    Q, R = np.polydiv(p, phid)
-    R = [x % q for x in R]
-    
-    return R
-
-def reduceMod_t(p):
-    # Put the given polynomial in the range [-0, t)
-    global phid
-    global t
-    p = [x % t for x in p]
-    # print(p)
-    Q, R = np.polydiv(p, phid)
-    R = [x % t for x in R]
-    
-    return R
 
 # def recurse(pos, d, myPol, polynomials):
 #     # Deprecated function to make every polynomial in Z[X]
-#     
+#
 #     global q
 #     if pos == d+1:
 #         # print(myPol)
@@ -88,7 +107,7 @@ def reduceMod_t(p):
 #         myPol.pop()
 
 # def genPolynomials(d, polynomials):
-#     # Deprecated Initialiser function to call recurse and 
+#     # Deprecated Initialiser function to call recurse and
 #     # set up polynomial space.
 #     global q
 #     myPol = []
@@ -115,7 +134,7 @@ def genProbabilities():
     probabilities = [x / sum for x in probabilities]
 
 def sample(B):
-    # Returns a random sample from -B+1 to B, according to the 
+    # Returns a random sample from -B+1 to B, according to the
     # probability space
     return np.random.choice(np.arange(-1*B+1, B), p=probabilities)
 
@@ -141,13 +160,13 @@ def ParamsGen():
     return d
 
 def inverse(f):
-    # Fast inverse method 
+    # Fast inverse method
     global phid
     global q
     p = ZZ.map(f)
     mod = ZZ.map(phid)
     s, t, g = gf_gcdex(p, mod, q, ZZ)
-    if len(g) == 1 and g[0] == 1: 
+    if len(g) == 1 and g[0] == 1:
         return s
     else:
         return [-1]
@@ -160,7 +179,7 @@ def KeyGen(d):
     g = ChiErr(d)
     f = [x*t for x in f_prime]
     f[-1] += 1
-    reduce(f)
+    reduce(f,q)
 
     # print("f_prime = ", f_prime)
     # print("g = ", g)
@@ -173,23 +192,23 @@ def KeyGen(d):
             f_prime = ChiErr(d)
             f = [x*t for x in f_prime]
             f[-1] += 1
-            reduce(f)
+            reduce(f,q)
             continue
         f_inv = [int(x) for x in X]
         if f_inv != [-1]:
             break
 
-    h = reduce(np.polymul(g, f_inv))
+    h = reduce(np.polymul(g, f_inv),q)
     h_prime = [x*t for x in h]
-    h = reduce(h_prime)
+    h = reduce(h_prime,q)
 
-    return f_prime, g, f_inv, reduce(h), reduce(f)
+    return f_prime, g, f_inv, reduce(h,q), reduce(f,q)
 
 def Encrypt(h, msg):
     global q
     global t
-    e = reduce(ChiErr(9)) #This shouldn't be 2, it should be some 
-    s = reduce(ChiKey(9)) #dependent of d, no?
+    e = reduce(ChiErr(9),q) #This shouldn't be 2, it should be some
+    s = reduce(ChiKey(9),q) #dependent of d, no?
 
 
     print("e = ", e)
@@ -198,20 +217,22 @@ def Encrypt(h, msg):
     delta = math.floor(q/t)
     c = [delta*x for x in msg]
     c = np.polyadd(c, e)
-    H = reduce(np.polymul(h, s))
+    H = reduce(np.polymul(h, s),q)
     c = np.polyadd(c, H)
-    return reduce(c)
+    return reduce(c,q)
 
 def Decrypt(f, c):
-    M = reduce(np.polymul(f, c))
+    global t
+    M = reduce(np.polymul(f, c),q)
     # print(M)
     M = [round(x*t/q) for x in M]
 
-    return reduce_t(M)
+    return reduce(M,t)
 
 def HomomorphicAddition(c1, c2):
+    global q
     A = np.polyadd(c1, c2)
-    return reduce(A)
+    return reduce(A,q)
 
 d = ParamsGen()
 genProbabilities()
@@ -224,7 +245,7 @@ f_prime, g, f_inv, h, f = KeyGen(d)
 # h = [-40, -36]
 # f_inv = [12, 53]
 # print("f_prime =",f_prime,"g =", g,"f_inv =", f_inv,"h =", h,"f =", f)
-f = reduce(f)
+f = reduce(f,q)
 # print("f_prime =",f_prime,"g =", g,"f_inv =", f_inv,"h =", h,"f =", f)
 
 # print("Enter message (a, b) to encrypt")
@@ -259,11 +280,11 @@ print("The messages entered are:")
 print("message1: ", msg1)
 print("message2: ", msg2)
 
-c1 = Encrypt(h, reduce_t(msg1))
-c2 = Encrypt(h, reduce_t(msg2))
+c1 = Encrypt(h, reduce(msg1,t))
+c2 = Encrypt(h, reduce(msg2,t))
 
 final_msg = Decrypt(f, HomomorphicAddition(c1, c2))
 print("First cipher text is: ", c1)
 print("Second cipher text is: ", c2)
-print("Homomorphic Addition is ", reduceMod_t(final_msg))
+print("Homomorphic Addition is ", reduce(final_msg,t,centered=False))
 # e = ChiErr
